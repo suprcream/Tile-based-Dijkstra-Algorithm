5e6d447991050d4db1eed0e4196631be9139d647
Suprcream <73229543+suprcream@users.noreply.github.com>
2023-12-24 13:29:42 +0700

    add first resource

---
+++ Assets/Scripts/Manager/MainManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    public GridManager gridManager;
+    public PathfindingManager pathfindingManager;
+}
